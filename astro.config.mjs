38098c778252be6830654952e8a14c4e5205131d
Denys Skobola <denysskobalodev@gmail.com>
2026-08-05 00:56:53 +0200

    fix(build): rename assets binding and enable nodejs_compat for cloudflare astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    assets: {
+      binding: 'CF_ASSETS',
+    },
+    platformProxy: {
+      enabled: true,
+      configPath: 'wrangler.toml',
+    },
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
