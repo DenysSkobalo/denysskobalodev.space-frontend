f2a0448e6ea3c3ff3a83829783d888a3a06f2a81
Denys Skobola <denysskobalodev@gmail.com>
2026-08-05 00:59:59 +0200

    fix(build): remove cloudflare adapter for static ssg build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare({
-    assets: {
-      binding: 'CF_ASSETS',
-    },
-    platformProxy: {
-      enabled: true,
-      configPath: 'wrangler.toml',
-    },
-  }),
   vite: {
     plugins: [tailwindcss()]
   },
